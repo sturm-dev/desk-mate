173addc2849450923f6005c9cdcd782f5cb3bd07
sturmenta <sturmenta@gmail.com>
2023-06-27 07:07:33 -0300

    src/app dir - bgAnimatedGradient

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import { BgAnimatedGradient } from "@/components";
+
+export default function Login() {
+  return (
+    <BgAnimatedGradient>
+      <div className="flex justify-center items-center h-screen w-screen">
+        <p className="text-5xl">Login</p>
+      </div>
+    </BgAnimatedGradient>
+  );
+}
