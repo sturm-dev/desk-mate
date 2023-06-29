df0c49164b8813ecc676829748c2ac994a7b40ce
sturmenta <sturmenta@gmail.com>
2023-06-29 12:25:59 -0300

    calendar_section

--- src/components/forThisApp/CalendarSection.tsx
+++ src/components/forThisApp/CalendarSection.tsx
@@ -1,7 +1,44 @@
 export const CalendarSection = () => {
   return (
-    <div className="flex flex-1 p-2">
-      <p>calendar section</p>
+    <div id="calendar_section_div" className="flex flex-1 p-2 flex-col">
+      {hours.map((hour, i) => (
+        <div
+          className={`${!isLastItem(i, hours) ? "border-b" : ""} flex flex-row`}
+          key={hour}
+          style={{ height: 1024 / hours.length }}
+        >
+          <div className="flex items-center justify-center h-full px-2 w-10">
+            <p>{hour}</p>
+          </div>
+          <div className="flex items-center flex-1 border-l p-1 pl-2">a</div>
+        </div>
+      ))}
     </div>
   );
 };
+
+// ─────────────────────────────────────────────────────────────────────────────
+
+const isLastItem = (index: number, array: any[]) => index === array.length - 1;
+
+const hours = [
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+];
