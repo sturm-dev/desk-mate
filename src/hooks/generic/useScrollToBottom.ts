51f87bcf24d1412996029df378a767a425ba4da6
sturmenta <sturmenta@gmail.com>
2023-06-30 16:34:58 -0300

    try to request full screen

--- src/hooks/generic/useScrollToBottom.ts
+++ src/hooks/generic/useScrollToBottom.ts
@@ -9,12 +9,25 @@ export const useScrollToBottom = ({ readyToRun }: { readyToRun: boolean }) => {
     if (!readyToRun || alreadyScrollToBottom) return;
 
     setTimeout(() => {
+      let elem = document.documentElement;
+
+      elem
+        .requestFullscreen({ navigationUI: "show" })
+        .then(() => {})
+        .catch((err) => {
+          alert(
+            `An error occurred while trying to switch into fullscreen mode: ${err.message} (${err.name})`
+          );
+        });
+    }, 2000);
+
+    setTimeout(() => {
       bottomDiv_ref?.current?.scrollIntoView({
         block: "end",
         behavior: "smooth",
       });
       setAlreadyScrollToBottom(true);
-    }, 2000);
+    }, 4000);
   }, [readyToRun]);
 
   return { bottomDiv_ref };
