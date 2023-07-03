7c22de387d7a3bcb17a5d23a8d2939c42ea20898
sturmenta <sturmenta@gmail.com>
2023-07-03 00:54:26 -0300

    final youtube bg video code

--- src/components/generic/YoutubeBgPlayer.tsx
+++ src/components/generic/YoutubeBgPlayer.tsx
@@ -1,48 +1,17 @@
 import YouTube, { YouTubeProps } from "react-youtube";
 
+const videoId = "V1bFr2SWP1I";
+
 export const YoutubeBgPlayer = () => {
-  const onPlayerReady: YouTubeProps["onReady"] = async (event) => {
-    console.log(`onPlayerReady - event.target`, event.target);
-
-    await sleep(100);
-    event.target.playVideo();
-
-    await sleep(100);
-    event.target.pauseVideo();
-
-    await sleep(100);
-    event.target.playVideo();
-  };
-  const onVideoEnd: YouTubeProps["onReady"] = async (event) => {
-    console.log(`onVideoEnd - event.target`, event.target);
-
-    await sleep(100);
-    event.target.playVideo();
-  };
-
   const opts: YouTubeProps["opts"] = {
-    height: "500",
-    width: "500",
-    playerVars: { autoplay: 1, mute: 1 },
+    height: "10",
+    width: "10",
+    playerVars: { autoplay: 1, mute: 1, playlist: videoId, loop: 1 },
   };
 
   return (
-    <YouTube
-      videoId="V1bFr2SWP1I"
-      opts={opts}
-      onReady={onPlayerReady}
-      onEnd={onVideoEnd}
-    />
+    <div className="absolute -z-10">
+      <YouTube videoId={videoId} opts={opts} />
+    </div>
   );
 };
-
-/* <iframe
-  width="200"
-  height="200"
-  src="https://www.youtube.com/embed/V1bFr2SWP1I?autoplay=1&mute=1&loop=1"
-/> */
-
-// ─────────────────────────────────────────────────────────────────────────────
-
-// sleep function
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
