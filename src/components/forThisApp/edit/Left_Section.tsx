2c1e678f01399467ed619d00182ac25a453a1360
sturmenta <sturmenta@gmail.com>
2023-07-18 21:57:40 -0300

    field to edit start

--- src/components/forThisApp/edit/Left_Section.tsx
+++ src/components/forThisApp/edit/Left_Section.tsx
@@ -1,7 +1,14 @@
+import { Touchable } from "@/components/generic";
+
 export const Left_Section = () => {
   return (
-    <div className="flex flex-1 border-r border-neutral-800 items-center justify-center">
-      <p>left</p>
+    <div className="flex flex-1 flex-col border-r border-neutral-800 items-center justify-center p-4">
+      <div className="flex flex-1 border border-neutral-800 w-full rounded-md p-4">
+        <p>field to edit</p>
+      </div>
+      <Touchable className="flex rounded-md mt-4 p-2 w-full items-center justify-center bg-green-800">
+        <p>Save</p>
+      </Touchable>
     </div>
   );
 };
